1e57b017fbbead3edff10182b5d99a2cbf06a95b
João-Paulo-Bortolocce <bbortolocce@gmail.com>
2024-10-11 22:17:35 -0300

    Continuando o desenvolvimento, OBS não compilei, há erros ainda

--- trabalho.cpp
+++ trabalho.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include <conio.h>
+#include<stdlib.h>
 #include<ctype.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -10,9 +11,12 @@
 #define TFP 10
 #define TFE 4
 
-//função para criar filhos --> FORK(FilaP[TFP],TpProcesso pai, *pids)
-//Função para colocar pai na fila de espera --> PaiToWait(FilaE[TFE],TpProcesso pai);
+//função para criar filhos --> FORK(FilaP[TFP],TpProcesso pai, pid_t &pids)
+//Função para colocar pai na fila de espera --> ProntoToWait(FilaE[TFE],TpProcesso pai,int tempoBloqueado);
 
+int sorteia(){
+	return rand()%100;
+}
 
 TpProcesso InicializarProcesso(){
 	TpProcesso proc;
@@ -75,24 +79,50 @@ int BuscaMaiorPrioridade(TpFilaPronto *FilaP[TFP]){
 	return i;
 }
 
+void DecrementaTempoBloqueado(TpFilaEspera *FilaE[TFE]){
+	int i;
+	TpFilaEspera *aux;
+	for(i=1;i<TFE;i++){
+		aux=FilaE[i];
+		while(aux!=NULL){
+			aux->PCB.tBloqueado--;
+			aux=aux->prox;
+		}
+	}
+}
+
+void WaitToProntoGeral(TpFilaEspera *FilaE[TFE],TpFilaPronto *FilaP[TFP]){
+	int i;
+	TpFilaEspera *aux;
+	TpProcesso processo;
+	for(i=1;i<TFE;i++){
+		aux=FilaE[i];
+		if(aux!=NULL && aux->PCB.tBloqueado==0){
+			FilaE[i]=dequeueEspera(FilaE[i],processo);
+			FilaP[processo.prior]=EnqueuePronto(FilaP[processo.prior],processo);
+		}
+	}
+}
+
 void Execucao(TpFilaPronto *FilaP[TFP],	TpFilaEspera *FilaE[TFE],pid_t &pids){
-	char flag=1;
+	char continua=1,flag=1;
 	int ut,maiorPrior=BuscaMaiorPrioridade(FilaP),qtdFinalizados=0;;
 	TpProcesso run;
 	FilaP[maiorPrior]=DequeuePronto(FilaP[maiorPrior],run);
 	run.estado='x';
-	while(flag){
+	while(continua){
 		ut=0;
 		while(!kbhit()){
 			if(run.tRestante==0){
 				qtdFinalizados++;
 				maiorPrior=BuscaMaiorPrioridade(FilaP);
 				if(maiorPrior>0){
+					//WaitToProntoPai(FilaP[run.prior],FilaE[0],run.ppid);//Função que retira o pai e coloca na fila de pronto, SE ELE TIVER PAI;
 					FilaP[maiorPrior]=DequeuePronto(FilaP[maiorPrior],run);
 					ut=0;
 				}
 				else
-					flag=0; // se for -1 quer dizer que todas as filas estão vazias, portanto pode finalizar a execucao
+					continua=0; // se for -1 quer dizer que todas as filas estão vazias, portanto pode finalizar a execucao
 			}
 			if(ut==10){
 				maiorPrior=BuscaMaiorPrioridade(FilaP);
@@ -102,7 +132,45 @@ void Execucao(TpFilaPronto *FilaP[TFP],	TpFilaEspera *FilaE[TFE],pid_t &pids){
 				}
 				ut=0;
 			}
-			//else{} No momento da troca de contexto o tempo de wait dos outros processos é diminuido?
+			else{//No momento da troca de contexto o tempo de wait dos outros processos é diminuido?
+				if(flag && sorteia()<3){
+					FORK(FilaP,run, pids);
+					run.tBloqueado=0;
+					enqueueEspera(FilaE[0],run);
+					maiorPrior=BuscaMaiorPrioridade(FilaP);
+					FilaP[maiorPrior]=DequeuePronto(FilaP[maiorPrior],run);
+					ut=0;
+				}
+				else{
+					if(sorteia()<3){
+						switch(sorteia()%3+1){
+							case 1:// Fila de mouse
+								run.tBloqueado=8;
+								enqueueEspera(FilaE[1],run);
+								break;
+							case 2:// Fila de Teclado
+								run.tBloqueado=15;
+								enqueueEspera(FilaE[2],run);
+								break;
+							case 3:// Fila de HD
+								run.tBloqueado=25;
+								enqueueEspera(FilaE[3],run);
+								break;
+						}
+						maiorPrior=BuscaMaiorPrioridade(FilaP);
+						while(maiorPrior<0){
+							DecrementaTempoBloqueado(FilaE);
+							WaitToProntoGeral(FilaE,FilaP);
+							maiorPrior=BuscaMaiorPrioridade(FilaP);
+						}
+						FilaP[maiorPrior]=DequeuePronto(FilaP[maiorPrior],run);
+						ut=0;
+					}
+					//FUNCIONAMENTO DE DECREMENTO E INCREMENTO DE TODOS OS TEMPOS.
+						
+				}
+				
+			} 
 		}
 	}
 }
